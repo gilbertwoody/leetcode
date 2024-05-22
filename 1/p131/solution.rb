7a23c0d7dbe2ea98b8e7693d483a0b5d3dcf20cb
gilbertwoody <gilbertwoody@outlook.com>
2024-05-22 20:56:58 +0800

    Solve problem 131, dynamic programming

--- 1/p131/solution.rb
+++ 1/p131/solution.rb
@@ -1,7 +1,31 @@
 # @param {String} s
 # @return {String[][]}
 def partition(s)
+  table = {}
+  split = lambda do |j, n|
+    return [[s[..j]]] if n.zero?
+    return [] if n > j
 
+    memo = table[[j, n]]
+    return memo if memo
+
+    p1 = split[j - 1, n]
+    p2 = split[j - 1, n - 1]
+
+    p1 = p1.map { |part| part[..-2] + [part[-1] + s[j]] }
+    p2 = p2.map { |part| part + [s[j]] }
+    table[[j, n]] = p1 + p2
+  end
+
+  n = s.size
+  result = []
+  (n - 1).downto(0) do |x|
+    p = split[n - 1, x]
+    result += p.filter do |part|
+      part.all? { |s| palindrome?(s) }
+    end
+  end
+  result
 end
 
 def palindrome?(s)
@@ -12,3 +36,4 @@ def palindrome?(s)
 
   return true
 end
+
